e3f08a707095dd7fbf69f104a846c5883d6f52fe
league <league@192.168.1.200>
2024-05-18 12:59:33 -0700

    working

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,28 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class LeagueInvaders {
-
+	
+	JFrame frame;	
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	GamePanel panel;
+	
+	public LeagueInvaders() {
+		frame = new JFrame();
+		panel = new GamePanel();
+	}
+	
+	void setup() {
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		frame.setVisible(true);
+		
+		
+	}
+	public static void main(String [] args) {
+		LeagueInvaders invaders = new LeagueInvaders();
+		invaders.setup();
+	}
 }
